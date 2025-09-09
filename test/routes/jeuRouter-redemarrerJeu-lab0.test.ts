41919b807f8e0651e80f5247acc684ed4cc89199
RemiCloutier <remicloutier2005@gmail.com>
2025-09-09 19:37:30 -0400

    étape 3

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,37 @@
 // Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
+import supertest from 'supertest'
+import app from '../../src/app';
 import 'jest-extended';
 
-describe('redemarrerJeu.test.ts', () => {
-  it("devrait implémenter test", async () => {
-    throw new Error("Ce test n'a pas été défini")
+const request = supertest(app);
+const testNom1 = "Rémi";
+const testNom2 = "Tristan";
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  beforeAll(async () => {
+    await request.post('api/v1/jeu/demarrerJeu').send({nom: testNom1})
+    await request.post('api/v1/jeu/demarrerJeu').send({nom: testNom2})
   });
+
+
+  it("scénario principal - succès : GET /api/v1/jeu/redemarrerJeu doit retourner 200 et du JSON", async () => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu');
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json");
+  });
+
+  it("postcondition : après redemarrerJeu, jouer/{nom} doit être 404 not found", async () => {
+    // redémarrer
+    await request.get('/api/v1/jeu/redemarrerJeu').expect(200);
+
+    // vérifier postcondition
+    const response = await request.get('/api/v1/jeu/jouer/' + testNom1);
+    expect(response.status).toBe(404);
+    expect(response.type).toBe("application/json");
+    expect(response.body.error).toInclude("n'existe pas");
+    expect(response.body.error).toInclude(testNom2);
+  });
+
 });
+
